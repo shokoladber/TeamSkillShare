6f04a50b154fae0e26dccbbef39c44414c71af48
codyhelm94 <codyhelm94@yahoo.com>
2022-04-18 15:25:56 -0500

    tested boostrap, MVC, and thymeleaf in HelloWorld Project

---
+++ src/main/java/com/skills/skills/models/User.java
@@ -0,0 +1,45 @@
+package com.skills.skills.models;
+
+
+import javax.persistence.Entity;
+
+@Entity
+public class User extends AbstractEntity {
+
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
