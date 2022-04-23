70e2176893d70b0d54debf573e3d1262f1fb197b
shokoladber <allen.r.michael@gmail.com>
2022-04-23 16:47:54 -0500

    Homepage, registration, login, up and running; built project structure including UserRepo, DTOs, and index, login, register htmls

--- src/main/java/com/skills/skills/models/dto/RegisterFormDTO.java
+++ src/main/java/com/skills/skills/models/dto/RegisterFormDTO.java
@@ -1,4 +1,15 @@
 package com.skills.skills.models.dto;
 
-public class RegisterFormDTO {
+public class RegisterFormDTO extends LoginFormDTO {
+
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+
 }
