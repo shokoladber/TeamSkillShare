bb3324aa030687fd27d7deeadd339a20f4812054
shokoladber <allen.r.michael@gmail.com>
2022-04-24 20:56:23 -0500

    Working on Load_registerForm_before_LoginForm

--- src/main/java/com/skills/skills/controllers/HomeController.java
+++ src/main/java/com/skills/skills/controllers/HomeController.java
@@ -1,15 +1,8 @@
 package com.skills.skills.controllers;
 
-import com.skills.skills.models.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
@@ -18,12 +11,6 @@ public class HomeController {
     //HomePage
     @RequestMapping("")
     public String index(Model model){
-        model.addAttribute("test","This is a Hello World Test");
-        model.addAttribute("people", Arrays.asList(
-                new User("John","Smith","test@aol.com"),
-                new User("Jane","Doe","test@google.com"),
-                new User("Richard","Roe", "test@yahoo.com")
-        ));
         return "index";
     }
 
