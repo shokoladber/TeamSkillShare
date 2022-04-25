8637b303c234bc5ca0f5b0bd9c7f177cf4d8b52e
shokoladber <allen.r.michael@gmail.com>
2022-04-24 20:56:23 -0500

    Working on Load_registerForm_before_LoginForm

--- src/main/java/com/skills/skills/controllers/HomeController.java
+++ src/main/java/com/skills/skills/controllers/HomeController.java
@@ -1,12 +1,8 @@
 package com.skills.skills.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
