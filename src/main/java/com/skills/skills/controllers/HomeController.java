c832cabad8bef436a7536956b48bf5da17ed1224
Fidel Rahmeto <fidyod@gmail.com>
2022-05-01 16:29:37 -0500

    adding userprofile

--- src/main/java/com/skills/skills/controllers/HomeController.java
+++ src/main/java/com/skills/skills/controllers/HomeController.java
@@ -1,30 +1,36 @@
 package com.skills.skills.controllers;
 
-import com.skills.skills.models.User;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
-
-@Controller
+//@Controller
+//@RequestMapping("")
 public class HomeController {
 
     //HomePage
-    @RequestMapping("")
-    public String index(Model model){
-        model.addAttribute("test","This is a Hello World Test");
-        model.addAttribute("people", Arrays.asList(
-                new User("John","Smith","test@aol.com"),
-                new User("Jane","Doe","test@google.com"),
-                new User("Richard","Roe", "test@yahoo.com")
-        ));
+  // @GetMapping
+    public String displayHomepage(Model model){
+       model.addAttribute("title","What is SkillShare");
+
         return "index";
-    }
+   }
 
 }
+
+
+
+
+
+
+
+
+
+//        model.addAttribute("test","This is a Hello World Test");
+//        model.addAttribute("people", Arrays.asList(
+//                new User("John","Smith","test@aol.com"),
+//                new User("Jane","Doe","test@google.com"),
+//                new User("Richard","Roe", "test@yahoo.com")
+//        ));
---
+++ src/main/java/com/skills/skills/data/UserRepository.java
@@ -0,0 +1,11 @@
+package com.skills.skills.data;
+
+import com.skills.skills.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    User findByUsername(String username);
+}
