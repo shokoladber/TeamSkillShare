375df5d0ec55c1feca891ada5ec17441c2584c05
wintaye <41543902+wintaye@users.noreply.github.com>
2022-05-24 18:36:07 -0500

    fixed path/redirect issue

--- src/main/java/com/skills/skills/controllers/AuthenticationController.java
+++ src/main/java/com/skills/skills/controllers/AuthenticationController.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -66,6 +63,17 @@ public class AuthenticationController {
         model.addAttribute(new Skill());
         model.addAttribute("tags", Tag.values());
         return "users/profile";
+    }
+
+    @GetMapping("/users/profile/{userId}")
+    public String displayPageAfterLogin ( @PathVariable Integer userId, HttpSession session, Model model) {
+        Optional<User> result = userRepository.findById(userId);
+        User user = result.get();
+        model.addAttribute("user", user);
+        model.addAttribute("skills", user.getSkills());
+        model.addAttribute(new Skill());
+        model.addAttribute("tags", Tag.values());
+        return "users/profile/{userId}";
     }
 
     @PostMapping("/users/profile")
