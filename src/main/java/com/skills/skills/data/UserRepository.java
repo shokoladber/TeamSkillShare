b04a0a119ab0f0a29ff89ddb1b6aeef95623573f
shokoladber <allen.r.michael@gmail.com>
2022-05-27 17:30:27 -0500

    Rebased event_functionality_rebase_onto_dev onto dev, modified imports to reflect changes; not working.

--- src/main/java/com/skills/skills/data/UserRepository.java
+++ src/main/java/com/skills/skills/data/UserRepository.java
@@ -4,8 +4,6 @@ import com.skills.skills.models.user.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
--- src/main/java/com/skills/skills/models/authentication/AuthenticationFilter.java
+++ src/main/java/com/skills/skills/models/authentication/AuthenticationFilter.java
@@ -1,4 +1,4 @@
-package com.skills.skills;
+package com.skills.skills.models.authentication;
 
 import com.skills.skills.controllers.AuthenticationController;
 import com.skills.skills.data.UserRepository;
