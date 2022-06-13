c00287a6cfe64388ea72a36cc655be026220d12a
curriated <92571896+curriated@users.noreply.github.com>
2022-06-13 16:21:01 -0500

    Styled profile, about, edit profile information. Checked each page for styling continuity.

--- src/main/java/com/skills/skills/models/LoadDatabase.java
+++ src/main/java/com/skills/skills/models/LoadDatabase.java
@@ -1,24 +1,24 @@
-//package com.skills.skills.models;
-//
-//
-//import com.skills.skills.data.SkillsCategoryRepository;
-//import com.skills.skills.data.TagRepository;
-//import com.skills.skills.models.skill.SkillsCategory;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class LoadDatabase {
-//
-//    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
-//
-//    @Bean
-//    CommandLineRunner initDatabase(SkillsCategoryRepository repository) {
-//
-//        return args -> {
+package com.skills.skills.models;
+
+
+import com.skills.skills.data.SkillsCategoryRepository;
+import com.skills.skills.data.TagRepository;
+import com.skills.skills.models.skill.SkillsCategory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoadDatabase {
+
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(SkillsCategoryRepository repository) {
+
+        return args -> {
 
 //            log.info("Preloading " + repository.save(new SkillsCategory("Charity/Non-Profit")));
 //            log.info("Preloading " + repository.save(new SkillsCategory("Ceramics")));
@@ -36,7 +36,7 @@
 //            log.info("Preloading " + repository.save(new SkillsCategory("Gardening")));
 //            log.info("Preloading " + repository.save(new SkillsCategory("Cooking")));
 //            log.info("Preloading " + repository.save(new SkillsCategory("Baking")));
-//
-//        };
-//    }
-//}
+
+        };
+    }
+}
